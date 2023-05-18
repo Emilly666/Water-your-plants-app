f84210badd89a4e70b34f9ed289b641975ff3f45
Emilia <kalanskae@gmail.com>
2023-05-18 14:40:20 +0200

    add:package for calendar

--- app/src/main/java/com/example/water_your_plants_app/ViewPagerAdapter.java
+++ app/src/main/java/com/example/water_your_plants_app/ViewPagerAdapter.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.example.water_your_plants_app.calendar.CalendarFragment;
 import com.example.water_your_plants_app.plants.PlantsFragment;
 import com.example.water_your_plants_app.tasks.HomeFragment;
 
--- app/src/main/java/com/example/water_your_plants_app/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/water_your_plants_app/calendar/CalendarFragment.java
@@ -1,4 +1,4 @@
-package com.example.water_your_plants_app;
+package com.example.water_your_plants_app.calendar;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.water_your_plants_app.R;
 
 public class CalendarFragment extends Fragment {
 
