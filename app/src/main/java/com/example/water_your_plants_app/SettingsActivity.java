35b2e1fa60773260544c45d6f3a06ef81ff291da
Emilia <kalanskae@gmail.com>
2023-06-17 17:10:15 +0200

    make settings use shared preferences

--- app/src/main/java/com/example/water_your_plants_app/SettingsActivity.java
+++ app/src/main/java/com/example/water_your_plants_app/SettingsActivity.java
@@ -1,20 +1,27 @@
 package com.example.water_your_plants_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.widget.TimePicker;
+
+import java.time.LocalTime;
 
 public class SettingsActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     TimePicker notificationsTimePicker;
+    SwitchCompat switchWater, switchFertilizer;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        context = getApplicationContext();
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -27,6 +34,14 @@ public class SettingsActivity extends AppCompatActivity {
             notificationsTimePicker.setIs24HourView(true);
         }
 
+        LocalTime notificationsTime = SharedPreferencesManager.getInstance(context).getNotificationsTime();
+        notificationsTimePicker.setHour(notificationsTime.getHour());
+        notificationsTimePicker.setMinute(notificationsTime.getMinute());
 
+        switchWater = findViewById(R.id.switchWater);
+        switchFertilizer = findViewById(R.id.switchFertilizer);
+
+        switchWater.setChecked(SharedPreferencesManager.getInstance(context).getNotificationsWater());
+        switchFertilizer.setChecked(SharedPreferencesManager.getInstance(context).getNotificationsFertilize());
     }
 }
