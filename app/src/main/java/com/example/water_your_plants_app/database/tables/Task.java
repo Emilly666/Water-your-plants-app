f6f9e1c6ec2310226fbef2ba72046b841cc5dd3d
NicoMroo <mroweczkan@gmail.com>
2023-06-27 16:06:42 +0200

    task update

--- app/src/main/java/com/example/water_your_plants_app/database/tables/Task.java
+++ app/src/main/java/com/example/water_your_plants_app/database/tables/Task.java
@@ -1,11 +1,26 @@
 package com.example.water_your_plants_app.database.tables;
 
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "tasks")
+@Entity(tableName = "tasks",
+        foreignKeys = @ForeignKey(entity = UserPlant.class, parentColumns = "userPlant_id", childColumns = "myUserPlant_id", onDelete = ForeignKey.CASCADE))
+
 public class Task {
 
     @PrimaryKey(autoGenerate = true)
     public long task_id;
+
+    public long myUserPlant_id;
+    public short taskType;
+
+    public String date;
+
+    public Task(long task_id, long myUserPlant_id, short taskType, String date ){
+        this.task_id = task_id;
+        this.myUserPlant_id = myUserPlant_id;
+        this.taskType = taskType;
+        this.date = date;
+    }
 }
